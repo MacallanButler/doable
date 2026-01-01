8ac535c6f981a8641839b0ba3cb8cab9ad32bd9e
Macallan Butler <macallan.butler@gmail.com>
2026-01-01 16:38:02 -0500

    Adding CRUD actions via Codealong

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -7,4 +7,43 @@ class ProjectsController < ApplicationController
     def show
         @project = Project.find(params[:id])
     end 
+
+    def new
+        @project = Project.new
+    end
+
+    def Create
+        @project = Project.new(project_params)
+        if @project.save
+            flash[:notice] = "Project was successfully created."
+            redirect_to project_path(@project)
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        @project = Project.find(params[:id])
+        if @project.update(project_params)
+            flash[:notice] = "Project was successfully updated."
+            redirect_to project_path(@project)
+        else
+            render :edit, status: :unprocessable_entity
+        end
+    end
+
+    def edit
+        @project = Project.find(params[:id])
+    end
+
+    def destroy
+        @project = Project.find(params[:id])
+        @project.destroy
+        flash[:notice] = "Project was successfully deleted."
+        redirect_to projects_path
+    end
+
+    def project_params
+        params.require(:project).permit(:name)
+    end
 end
